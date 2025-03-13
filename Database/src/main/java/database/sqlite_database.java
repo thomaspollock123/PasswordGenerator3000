4cc18efdad6ff0e07226eb1566d110a822ecc6e0
thomaspollock123 <thomas.pollock@ucdconnect.ie>
2025-03-13 00:39:41 +0000

    Basic JavaFX set up

---
+++ Database/src/main/java/database/sqlite_database.java
@@ -0,0 +1,4 @@
+package database;
+
+public class sqlite_database {
+}
--- UI/src/main/java/user_page/JavaFX_Hello.java
+++ UI/src/main/java/user_page/JavaFX_Hello.java
@@ -2,8 +2,12 @@ package user_page;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
@@ -16,21 +20,23 @@ public class JavaFX_Hello extends Application {
             stage.setTitle("This is a Test VBox");
 
             // Create VBox for each corner
-            VBox Top = createVBox("Top");
-            VBox Bottom = createVBox("Bottom");
+            VBox Top = createVBox(10, "TOP_LEFT", "Password Input", "Press me :D");
+            VBox Bottom = createVBox(10, "BOTTOM_LEFT", "Password Generator", "Enter");
+            TableView right = new TableView();
 
             // Create a GridPane and place VBoxes in four corners
             GridPane grid = new GridPane();
             grid.setHgap(10); // Horizontal gap between VBoxes
             grid.setVgap(10); // Vertical gap between VBoxes
-            grid.setPadding(new Insets(0, 0, 0, 10));
+            grid.setPadding(new Insets(10));
 
             // Place VBoxes in four corners of the grid
             grid.add(Top, 0, 0);
             grid.add(Bottom, 0, 1);
+            grid.add(right, 1, 0, 1,2);
             grid.setAlignment(Pos.BASELINE_LEFT);
 
-            Scene scene = new Scene(grid, 400, 200);
+            Scene scene = new Scene(grid, 350, 300);
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
@@ -39,13 +45,13 @@ public class JavaFX_Hello extends Application {
     }
 
     // Helper method to create a VBox
-    private VBox createVBox(String labelText) {
-        VBox vbox = new VBox(10);
-        Label l = new Label("This is " + labelText);
-        Button b = new Button("Press me :D");
+    private VBox createVBox(int size, String position, String lab, String but) {
+        VBox vbox = new VBox(size);
+        Label l = new Label(lab);
+        Button b = new Button(but);
         TextField t = new TextField();
         vbox.getChildren().addAll(l, t, b);
-        vbox.setAlignment(Pos.BASELINE_LEFT);
+        vbox.setAlignment(Pos.valueOf(position.toUpperCase()));
         return vbox;
     }
 
